4766755d3774c2fa91818c31c5193f5e39690349
codepiya <155807332+codepiya@users.noreply.github.com>
2024-06-24 07:01:36 +0000

    2nd day notes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 13355
+let accountEmail = "pruiya@hoogle.com"
+var accountPassword = "1245"
+accountCity = "delhi"
+
+//accountId = 2//not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "676789"
+accountCity = "hindustan"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";// treat all JS code as newe version 
+
+//alert( 3 + 3)// we are using nodejs, not browser
+
+//datatypes:-
+
+let name ="Priya"//string datatype
+let age = 18//number
+let isLoggedIn = false //boolean type
+let state
+
+//number => 2 to power 53
+//bigint
+//string => " "
+//boolean => true/false
+//null => standalone value
+//undefined => not defined yet // null and undefined are different.
+//ysmbol => unique 
+
+
+//object
+
+console.log(typeof undefined);//undefined type
+console.log(typeof null);//object  type
