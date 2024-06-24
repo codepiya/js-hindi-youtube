389c5b72fbc64556b5abf0211f5957b6f2e27350
codepiya <155807332+codepiya@users.noreply.github.com>
2024-06-24 19:19:42 +0000

    conversion of datatypes'

---
+++ 01_basic/03_conversionOperations.js
@@ -0,0 +1,28 @@
+/*let score = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+*/
+
+//"33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1; false => 0
+
+/*let isLoggedIn = "Priya"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+*/
+
+// 1 => true
+// "" => false
+// "Priya" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
